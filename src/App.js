599813c392398347b22126278cc3fffe034f64d7
Nicole Martin <n.martinpdx@gmail.com>
2021-11-21 14:38:25 -0800

    add router path

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import './App.css';
 import Canvas from './containers/Canvas';
+import Nav from './components/UI/Nav';
+import Footer from './components/UI/Footer';
+import Home from './containers/Home';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  BrowserRouter,
+} from 'react-router-dom';
 
 function App() {
-    return (
-        <>
-            <Canvas />
-        </>
-    );
+  return (
+    <>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/draw" element={<Canvas />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
