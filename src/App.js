eab685449d5c3cd25e3d6174e588ac6e27e96968
kb <katrinkjb@gmail.com>
2021-11-24 12:40:18 -0800

    commenting out gallery and save functionality. Started background styling

--- src/App.js
+++ src/App.js
@@ -3,22 +3,22 @@ import Nav from './components/UI/Nav';
 import Footer from './components/UI/Footer';
 import Home from './containers/Home';
 import Canvas from './containers/Canvas';
-import Gallery from './containers/Gallery';
+// import Gallery from './containers/Gallery';
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/draw" element={<Canvas />} />
-          <Route path="/gallery" element={<Gallery />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter>
+                <Nav />
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/draw' element={<Canvas />} />
+                    {/* <Route path="/gallery" element={<Gallery />} /> */}
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,13 @@
 import './styles/Home.css';
-
 import React from 'react';
+import background from '../assets/QuickBackground.png';
 
 const Home = () => {
     return (
-        <main className='mainSection'>
+        <main
+            className='mainSection'
+            style={{ backgroundImage: `url(${background})` }}
+        >
             <div>
                 <h2>HOME</h2>
             </div>
