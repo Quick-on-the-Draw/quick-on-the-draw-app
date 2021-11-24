28f75808f3c1a515a3090bb19413426c38f6fc7d
Nicole Martin <n.martinpdx@gmail.com>
2021-11-24 14:21:17 -0800

    some style to canvas, not done

--- src/components/Buttons/ControlButtons.jsx
+++ src/components/Buttons/ControlButtons.jsx
@@ -2,22 +2,24 @@ import React from 'react';
 import './styles/ControlButtons.css';
 
 const ControlButtons = ({ handleUndo, handleClear, handleDownload }) => {
-    return (
-        <>
-            <button className='canvasBtn' onClick={handleUndo}>
-                Undo
-            </button>
-            <button className='canvasBtn' onClick={handleClear}>
-                Clear
-            </button>
-            <button className='canvasBtn' onClick={handleDownload}>
-                Download
-            </button>
-            {/* <button className="canvasBtn" onClick={handleSave}>
-        SAVE
-      </button> */}
-        </>
-    );
+  return (
+    <>
+      <button className="canvasBtn" onClick={handleUndo}>
+        Undo
+      </button>
+      <button className="canvasBtn" onClick={handleClear}>
+        Clear
+      </button>
+      <a
+        className="canvasBtn"
+        href="my drawing"
+        download="drawing.png"
+        onClick={handleDownload}
+      >
+        Download
+      </a>
+    </>
+  );
 };
 
 export default ControlButtons;
--- src/containers/Canvas.jsx
+++ src/containers/Canvas.jsx
@@ -9,107 +9,99 @@ import './styles/Canvas.css';
 import background from '../assets/QuickBackground.png';
 
 const Canvas = () => {
-    const [brushColor, setBrushColor] = useState('#000000');
-    const [brushSize, setBrushSize] = useState(1);
-    // const [loading, setLoading] = useState(true);
+  const [brushColor, setBrushColor] = useState('#000000');
+  const [brushSize, setBrushSize] = useState(1);
+  // const [loading, setLoading] = useState(true);
 
-    const canvasRef = useRef();
-    const canvasProps = {
-        lazyRadius: 0,
-        brushColor: brushColor,
-        brushRadius: brushSize,
-        hideInterface: true,
-        hideGrid: true,
-        canvasWidth: 700,
-        canvasHeight: 700,
-    };
+  const canvasRef = useRef();
+  const canvasProps = {
+    lazyRadius: 0,
+    brushColor: brushColor,
+    brushRadius: brushSize,
+    hideInterface: true,
+    hideGrid: true,
+    canvasWidth: 700,
+    canvasHeight: 700,
+  };
 
-    const handleColorChange = (color) => {
-        setBrushColor(color);
-    };
+  const handleColorChange = (color) => {
+    setBrushColor(color);
+  };
 
-    const handleUndo = () => {
-        canvasRef.current.undo();
-    };
+  const handleUndo = () => {
+    canvasRef.current.undo();
+  };
 
-    const handleClear = () => {
-        canvasRef.current.clear();
-    };
+  const handleClear = () => {
+    canvasRef.current.clear();
+  };
 
-    const handleDownload = (e) => {
-        const dataURL = canvasRef.current.getDataURL({
-            fileType: 'png',
-            useBgImage: true,
-            backgroundColour: '#ffffff',
-        });
-        e.target.href = dataURL;
-        console.log(dataURL);
-    };
+  const handleDownload = (e) => {
+    const dataURL = canvasRef.current.getDataURL({
+      fileType: 'png',
+      useBgImage: true,
+      backgroundColour: '#ffffff',
+    });
+    e.target.href = dataURL;
+    console.log(dataURL);
+  };
 
-    // const handleSave = (e) => {
-    //     console.log('saved!!!');
-    //     e.preventDefault();
-    //     const saveDataString = canvasRef.current.getSaveData();
-    //     console.log(JSON.parse(saveDataString));
-    //     // const form = new FormData();
-    //     // const dataUrl = canvasRef.current.getDataURL();
-    //     // // console.log(dataUrl);
-    //     // form.append('url', dataUrl);
-    //     // form.append('title', 'testing');
-    //     // form.append('createdDate', '1999-01-09');
-    //     // form.append('timerSetting', 5);
-    //     // addDrawing(form);
-    //     // console.log('the end');
-    //     addDrawing({
-    //         title: 'title',
-    //         createdDate: '1999-01-09',
-    //         timerSetting: 5,
-    //         url: saveDataString,
-    //     });
-    //     // console.log(dataUrl);
-    // };
+  // const handleSave = (e) => {
+  //     console.log('saved!!!');
+  //     e.preventDefault();
+  //     const saveDataString = canvasRef.current.getSaveData();
+  //     console.log(JSON.parse(saveDataString));
+  //     // const form = new FormData();
+  //     // const dataUrl = canvasRef.current.getDataURL();
+  //     // // console.log(dataUrl);
+  //     // form.append('url', dataUrl);
+  //     // form.append('title', 'testing');
+  //     // form.append('createdDate', '1999-01-09');
+  //     // form.append('timerSetting', 5);
+  //     // addDrawing(form);
+  //     // console.log('the end');
+  //     addDrawing({
+  //         title: 'title',
+  //         createdDate: '1999-01-09',
+  //         timerSetting: 5,
+  //         url: saveDataString,
+  //     });
+  //     // console.log(dataUrl);
+  // };
 
-    // if (loading) return 'LOADING';
+  // if (loading) return 'LOADING';
 
-    return (
-        <main
-            className='main'
-            style={{ backgroundImage: `url(${background})` }}
-        >
-            <div className='container'>
-                <Timer />
-                <div>
-                    <CanvasDraw
-                        {...canvasProps}
-                        className='mainCanvas'
-                        ref={canvasRef}
-                    />
-                </div>
-                <div>
-                    <ColorPalette
-                        pickColor={handleColorChange}
-                        setBrushColor={setBrushColor}
-                    />
-                    <BrushSize setBrushSize={setBrushSize} />
-                    <ControlButtons
-                        handleUndo={handleUndo}
-                        handleClear={handleClear}
-                        handleDownload={handleDownload}
-                        // handleSave={handleSave}
-                    />
-
-                    {/* <a
-                        className='download-link'
-                        href='my drawing'
-                        download='drawing.png'
-                        onClick={handleDownload}
-                    >
-                        Download
-                    </a> */}
-                </div>
-            </div>
-        </main>
-    );
+  return (
+    <main className="main" style={{ backgroundImage: `url(${background})` }}>
+      <div className="container">
+        <Timer />
+        <ControlButtons
+          className="controlButtons"
+          handleUndo={handleUndo}
+          handleClear={handleClear}
+          handleDownload={handleDownload}
+          // handleSave={handleSave}
+        />
+        <div className="sideContainer">
+          <div className="canvasContainer">
+            <CanvasDraw
+              {...canvasProps}
+              className="mainCanvas"
+              ref={canvasRef}
+            />
+          </div>
+          <div className="colorContainer">
+            <BrushSize setBrushSize={setBrushSize} />
+            <ColorPalette
+              className="colorPalette"
+              pickColor={handleColorChange}
+              setBrushColor={setBrushColor}
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Canvas;
