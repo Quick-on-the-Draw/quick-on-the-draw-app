cec01e57561503a0d4cada1ff1a8adb660a31afa
kb <katrinkjb@gmail.com>
2021-11-29 17:52:30 -0800

    added icons

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -17,8 +17,10 @@ const Home = () => {
                 <p>
                     Click Draw to go to the Draw page. Choose your timer
                     setting, then choose your colors and brush size at the
-                    bottom of the screen. Undo or Clear when you need and
-                    Download your image when you're finished!
+                    bottom of the screen.
+                    <br />
+                    Undo or Clear when you need and Download your image when
+                    you're finished!
                 </p>
                 <h3 className='headerHome'>Technologies:</h3>
                 <p>
